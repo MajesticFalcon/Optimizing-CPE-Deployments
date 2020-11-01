e0f3997e7037da05659b9d06cf6c1f6a2028951b
Schylar Utley <schylarutley@hotmail.com>
2020-11-01 18:56:41 +0000

    conform to pep8

--- flask/app.py
+++ flask/app.py
@@ -9,26 +9,30 @@ from flask import request
 app = Flask(__name__)
 
 
-    
-    
-    
-    
-
 @app.route('/create_configuration', methods=['POST'])
 def create_configuration():
     device_name = request.json["data"]["name"]
-    os.system("ansible-playbook -i /optimizing_cpes/ansible/netbox_inventory.yml /optimizing_cpes/ansible/create_configuration.yml -e 'device_name={0}'".format(device_name))
+    os.system("ansible-playbook \
+              -i /optimizing_cpes/ansible/netbox_inventory.yml \
+              /optimizing_cpes/ansible/create_configuration.yml \
+              -e 'device_name={0}'".format(device_name))
     os.system("git -C /optimizing_cpes/gitlab add .")
-    os.system("git -C /optimizing_cpes/gitlab commit -am 'auto commit from webhook'")
+    os.system("git \
+              -C /optimizing_cpes/gitlab commit \
+              -am 'auto commit from webhook'")
     os.system("git -C /optimizing_cpes/gitlab push origin master")
     return("/")
+
+
 @app.route('/test_ocnos', methods=['POST'])
 def test_ocnos():
-    os.system("ansible-playbook -i /optimizing_cpes/ansible/netbox_inventory.yml /optimizing_cpes/ansible/ocnos_test.yml")
+    os.system("ansible-playbook \
+              -i /optimizing_cpes/ansible/netbox_inventory.yml \
+              /optimizing_cpes/ansible/ocnos_test.yml")
     return("/")
-    
+
+
 if __name__ == '__name__':
     app.run()
 
-app.run(host='0.0.0.0',port=5008)
-
+app.run(host='0.0.0.0', port=5008)
